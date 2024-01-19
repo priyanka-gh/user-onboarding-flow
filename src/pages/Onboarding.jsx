034cd40e66cc0edfeeab75fe64ddd8b0f71eac6d
Priyanka Ghansela <priyanka.ghanselaa@gmail.com>
2024-01-19 22:01:27 +0530

    Responsiveness

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -15,14 +15,14 @@ const Onboarding = () => {
 
   return (
     <div className="h-[100vh] grid place-items-center place-content-center">
-      <div className='flex flex-col items-center justify-center gap-10 fixed top-20'>
+      <div className='flex flex-col items-center justify-center gap-10 fixed top-[15%]'>
         <div className='flex gap-2 items-center justify-center '>
           <img src={logo} alt="" />
           <h1 className='text-2xl font-bold font-poppins'>Eden</h1>
         </div>
         <Stepper currentPage={currentPage} totalPages={totalPages}/>
       </div>
-      <div className="p-8 flex flex-col relative top-14">
+      <div className="p-8 flex flex-col relative top-[20%]">
         <FormPage nextPage={nextPage}/>
       </div>
     </div>
